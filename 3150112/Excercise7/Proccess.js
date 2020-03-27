c225010c858bc613b401078f1e4d07c67543e1c4
ManosMorf97 <manosmorf97@gmail.com>
2020-03-27 20:43:58 +0200

    Commit

---
+++ 3150112/Excercise7/Proccess.js
@@ -0,0 +1,35 @@
+let definitions=['definition1','definition2'];
+let colors=['darkblue','blue'];
+let hide=true;
+const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+   "Saturday"
+];
+
+const d = new Date();
+if (dayNames[d.getDay()]=="Sunday")
+	document.getElementsByTagName('body')[0].style.backgroundColor="Crimson";
+else if (dayNames[d.getDay()]=="Monday")
+	document.getElementsByTagName('body')[0].style.backgroundColor="Coral";
+else if  (dayNames[d.getDay()]=="Tuesday")
+	document.getElementsByTagName('body')[0].style.backgroundColor="CadetBlue";
+else if  (dayNames[d.getDay()]=="Wednesday")
+	document.getElementsByTagName('body')[0].style.backgroundColor="CornflowerBlue";
+else if  (dayNames[d.getDay()]=="Thursday")
+	document.getElementsByTagName('body')[0].style.backgroundColor="Chartreuse";
+else if  (dayNames[d.getDay()]=="Friday")
+	document.getElementsByTagName('body')[0].style.backgroundColor="Aquamarine";
+else 
+	document.getElementsByTagName('body')[0].style.backgroundColor="Aqua";
+function DisappearContent(){
+	if(hide){
+		for(let i=0; i<2; i++)
+			document.getElementById(definitions[i]).innerHTML="<p>You disappeared the contents.</p>"
+		document.getElementById("Hide").innerHTML="Im useless now"
+    }
+    hide=!hide;
+}
+function swapColors(){
+	for(let i=0; i<2; i++)
+		document.getElementById(definitions[i]).style.backgroundColor=colors[i];
+	colors=[colors[1],colors[0]];
+}
