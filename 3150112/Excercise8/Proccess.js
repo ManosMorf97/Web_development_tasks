d0d307ab2c9234599164f0a974a81e1291c10d22
ManosMorf97 <manosmorf97@gmail.com>
2020-04-05 18:59:19 +0300

    Commit

--- 3150112/Excercise8/Proccess.js
+++ 3150112/Excercise8/Proccess.js
@@ -4,3 +4,6 @@ function hovered(x){
 function unhovered(x){
 	x.style.backgroundColor="darkKhaki";
 }
+let defs=document.getElementsByClassName("definition1");
+for(let i=0; i<defs.length; i++)
+	document.getElementsByClassName("definition1")[i].style.backgroundColor="green";
